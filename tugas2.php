4ae64409e5e9f95580d0e092b94d923d7d5e0fa5
FergiPutraAbiansa31 <fergiputra257@gmail.com>
2023-10-26 18:55:25 +0800

    Tugas 2 php

---
+++ tugas2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    for ($i = 9; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
